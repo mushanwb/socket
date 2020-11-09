2746e20a02be35dd9db4f29031fa31306d524f4b
wangbo <1216476406@qq.com>
2020-11-09 17:35:13 +0800

    当客户端第一次连接服务端的时候，需要记录客户端信息

--- src/main/java/com/github/mushanwb/ClientConnection.java
+++ src/main/java/com/github/mushanwb/ClientConnection.java
@@ -1,5 +1,8 @@
 package com.github.mushanwb;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 // 当一个客户端连上服务端 用多线程的方式读写数据
@@ -7,15 +10,40 @@ public class ClientConnection extends Thread {
 
     private Socket socket;
 
+    // 每当有一个客户端连上的时候，
+    // 需要有一个 客户端昵称 和 id，来标识是哪一个客户端（重点是 id）
+    private String clientName;
+    private Integer clientId;
+    private Server server;
+
+
     /**
      * @param socket 接收一个 socket 数据
      */
-    public ClientConnection(Socket socket) {
+    public ClientConnection(Socket socket,
+                            Server server) {
         this.socket = socket;
+        this.server = server;
     }
 
     @Override
     public void run() {
-        super.run();
+        try {
+            // 从 socket 数据中读取我们需要的数据
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line = null;
+            // 循环读取流中的数据
+            while ((line = reader.readLine()) != null) {
+                // 第一次连上服务端的时候，将昵称发送给服务端
+                if (clientName == null) {
+                    clientName = line;
+                    server.registerClient(this);
+                } else {
+                    // 不是第一次连接的时候，将获取客户端发送的数据，并且转发给另外一个客户端
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/github/mushanwb/Server.java
+++ src/main/java/com/github/mushanwb/Server.java
@@ -3,12 +3,17 @@ package com.github.mushanwb;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 // 服务端
 public class Server {
 
     // 需要一个 ServerSocket 服务
     private final ServerSocket server;
+    private final Map<Integer, ClientConnection> clients = new ConcurrentHashMap<>();
+    private static AtomicInteger COUNTER = new AtomicInteger(0);
 
     /**
      * @param port TCP 连接的端口号，0 ~ 65535
@@ -28,12 +33,22 @@ public class Server {
             // 使用多线程的方式来读取客户端发送的数据
             // 如果不用多线程方式读取的话，当服务端正在读取一个用户的信息时，accept 方法不执行
             // 这样会导致其他用户将连接不进来，因此我们需要将读取信息的操作交给多线程处理
-            new ClientConnection(socket).start();
+            new ClientConnection(socket, this).start();
         }
     }
 
     public static void main(String[] args) throws IOException {
         // 使用 8081 端口，并启动死循环监听
         new Server(8081).start();
+    }
+
+    /**
+     * 注册用户
+     * 每次有客户端连接上来的时候，将客户端的 id 自增 1，客户端 id 从 1 开始
+     * 当服务端发送消息选择的客户端 id 为 0 时，则代表给所有人发送消息，否则给某一个指定客户端发消息
+     * @param clientConnection 连接的客户端
+     */
+    public void registerClient(ClientConnection clientConnection) {
+        clients.put(COUNTER.incrementAndGet(), clientConnection);
     }
 }
